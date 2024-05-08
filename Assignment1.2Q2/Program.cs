f36863dd08356e41e4b498d9137f61350c8f2405
flowmellow <michael.floresmmt@gmail.com>
2024-05-08 16:20:12 -0400

    Add project files.

---
+++ Assignment1.2Q2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Assignment1._2Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Brute force
+            int num1 = 1;
+            int num2 = 2;
+            int num3 = 3;
+            int num4 = 4;
+            int num5 = 5;
+            int num6 = 6;
+            int num7 = 7;
+            int num8 = 8;
+            int num9 = 9;
+            int num10 = 10;
+
+            Console.WriteLine("The first 10 natural numbers are:");
+            Console.WriteLine("1 2 3 4 5 6 7 8 9 10");
+            Console.WriteLine("The Sum is: " + (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10));
+
+            // for loop version
+            //int sum = 0;
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+        }
+    }
+}
